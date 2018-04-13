c089b6106a76929c4577b39418428508396beaa7
Quagon <jonash_msn@hotmail.com>
2018-04-13 21:15:43 +0200

    Fixed the solution, Ordered some stuff into folders for better management, Created a landingpage which directs to a loginpage

---
+++ LandingPage/LandingPageForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace LandingPage
+{
+    partial class LandingPageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LandingPageWelcome = new System.Windows.Forms.Label();
+            this.LandingPageDescription1 = new System.Windows.Forms.Label();
+            this.LandingPageRegister = new System.Windows.Forms.Button();
+            this.LandingPageLogin = new System.Windows.Forms.Button();
+            this.LandingPageDescription2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LandingPageWelcome
+            // 
+            this.LandingPageWelcome.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LandingPageWelcome.AutoSize = true;
+            this.LandingPageWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LandingPageWelcome.Location = new System.Drawing.Point(172, 39);
+            this.LandingPageWelcome.Name = "LandingPageWelcome";
+            this.LandingPageWelcome.Size = new System.Drawing.Size(126, 31);
+            this.LandingPageWelcome.TabIndex = 0;
+            this.LandingPageWelcome.Text = "Welcome";
+            // 
+            // LandingPageDescription1
+            // 
+            this.LandingPageDescription1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LandingPageDescription1.AutoSize = true;
+            this.LandingPageDescription1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LandingPageDescription1.Location = new System.Drawing.Point(55, 97);
+            this.LandingPageDescription1.MaximumSize = new System.Drawing.Size(400, 0);
+            this.LandingPageDescription1.Name = "LandingPageDescription1";
+            this.LandingPageDescription1.Size = new System.Drawing.Size(376, 65);
+            this.LandingPageDescription1.TabIndex = 1;
+            this.LandingPageDescription1.Text = "Welcome To our Health Application. We will try to help you keep track of your Wei" +
+    "ght and Calories.\r\n\r\nHowever, before we can help you, we would need a bit of inf" +
+    "ormation about yourself.";
+            this.LandingPageDescription1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LandingPageRegister
+            // 
+            this.LandingPageRegister.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LandingPageRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LandingPageRegister.Location = new System.Drawing.Point(131, 202);
+            this.LandingPageRegister.Name = "LandingPageRegister";
+            this.LandingPageRegister.Size = new System.Drawing.Size(223, 29);
+            this.LandingPageRegister.TabIndex = 2;
+            this.LandingPageRegister.Text = "Register";
+            this.LandingPageRegister.UseVisualStyleBackColor = true;
+            this.LandingPageRegister.Click += new System.EventHandler(this.LandingPageRegister_Click);
+            // 
+            // LandingPageLogin
+            // 
+            this.LandingPageLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LandingPageLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LandingPageLogin.Location = new System.Drawing.Point(131, 364);
+            this.LandingPageLogin.Name = "LandingPageLogin";
+            this.LandingPageLogin.Size = new System.Drawing.Size(223, 29);
+            this.LandingPageLogin.TabIndex = 3;
+            this.LandingPageLogin.Text = "Login";
+            this.LandingPageLogin.UseVisualStyleBackColor = true;
+            this.LandingPageLogin.Click += new System.EventHandler(this.LandingPageLogin_Click);
+            // 
+            // LandingPageDescription2
+            // 
+            this.LandingPageDescription2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LandingPageDescription2.AutoSize = true;
+            this.LandingPageDescription2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LandingPageDescription2.Location = new System.Drawing.Point(74, 337);
+            this.LandingPageDescription2.MaximumSize = new System.Drawing.Size(400, 0);
+            this.LandingPageDescription2.Name = "LandingPageDescription2";
+            this.LandingPageDescription2.Size = new System.Drawing.Size(336, 13);
+            this.LandingPageDescription2.TabIndex = 4;
+            this.LandingPageDescription2.Text = "If you already happen to have an account, you can always login here:";
+            this.LandingPageDescription2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LandingPageForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.LandingPageDescription2);
+            this.Controls.Add(this.LandingPageLogin);
+            this.Controls.Add(this.LandingPageRegister);
+            this.Controls.Add(this.LandingPageDescription1);
+            this.Controls.Add(this.LandingPageWelcome);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "LandingPageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diet Tracker v.0.1";
+            this.Load += new System.EventHandler(this.LandingPageForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LandingPageWelcome;
+        private System.Windows.Forms.Label LandingPageDescription1;
+        private System.Windows.Forms.Button LandingPageRegister;
+        private System.Windows.Forms.Button LandingPageLogin;
+        private System.Windows.Forms.Label LandingPageDescription2;
+    }
+}
+
---
+++ UserLogin/LoginPage/LoginForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Login
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.nametxtbox = new System.Windows.Forms.TextBox();
+            this.pwdtxtbox = new System.Windows.Forms.TextBox();
+            this.Forgot_password = new System.Windows.Forms.LinkLabel();
+            this.LoginPage = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.LoginDescription = new System.Windows.Forms.Label();
+            this.RegisterDescription = new System.Windows.Forms.Label();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.LoginPageBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Location = new System.Drawing.Point(121, 241);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(223, 29);
+            this.LoginButton.TabIndex = 2;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // nametxtbox
+            // 
+            this.nametxtbox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nametxtbox.Location = new System.Drawing.Point(32, 148);
+            this.nametxtbox.MaximumSize = new System.Drawing.Size(500, 20);
+            this.nametxtbox.MinimumSize = new System.Drawing.Size(100, 20);
+            this.nametxtbox.Name = "nametxtbox";
+            this.nametxtbox.Size = new System.Drawing.Size(400, 20);
+            this.nametxtbox.TabIndex = 3;
+            // 
+            // pwdtxtbox
+            // 
+            this.pwdtxtbox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pwdtxtbox.Location = new System.Drawing.Point(32, 206);
+            this.pwdtxtbox.MaximumSize = new System.Drawing.Size(500, 20);
+            this.pwdtxtbox.MinimumSize = new System.Drawing.Size(100, 20);
+            this.pwdtxtbox.Name = "pwdtxtbox";
+            this.pwdtxtbox.PasswordChar = '*';
+            this.pwdtxtbox.Size = new System.Drawing.Size(400, 20);
+            this.pwdtxtbox.TabIndex = 4;
+            // 
+            // Forgot_password
+            // 
+            this.Forgot_password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Forgot_password.AutoSize = true;
+            this.Forgot_password.Location = new System.Drawing.Point(184, 284);
+            this.Forgot_password.Name = "Forgot_password";
+            this.Forgot_password.Size = new System.Drawing.Size(92, 13);
+            this.Forgot_password.TabIndex = 5;
+            this.Forgot_password.TabStop = true;
+            this.Forgot_password.Text = "Forgot Password?";
+            this.Forgot_password.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Forgot_password_LinkClicked);
+            // 
+            // LoginPage
+            // 
+            this.LoginPage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginPage.AutoSize = true;
+            this.LoginPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginPage.Location = new System.Drawing.Point(153, 35);
+            this.LoginPage.Name = "LoginPage";
+            this.LoginPage.Size = new System.Drawing.Size(150, 31);
+            this.LoginPage.TabIndex = 7;
+            this.LoginPage.Text = "Login Page";
+            this.LoginPage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Username
+            // 
+            this.Username.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(28, 125);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(87, 20);
+            this.Username.TabIndex = 8;
+            this.Username.Text = "Username:";
+            // 
+            // Password
+            // 
+            this.Password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(28, 183);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(82, 20);
+            this.Password.TabIndex = 9;
+            this.Password.Text = "Password:";
+            // 
+            // LoginDescription
+            // 
+            this.LoginDescription.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginDescription.AutoSize = true;
+            this.LoginDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginDescription.Location = new System.Drawing.Point(89, 90);
+            this.LoginDescription.Name = "LoginDescription";
+            this.LoginDescription.Size = new System.Drawing.Size(284, 13);
+            this.LoginDescription.TabIndex = 10;
+            this.LoginDescription.Text = "Use your Username and Password to log into your account";
+            // 
+            // RegisterDescription
+            // 
+            this.RegisterDescription.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RegisterDescription.AutoSize = true;
+            this.RegisterDescription.Enabled = false;
+            this.RegisterDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterDescription.Location = new System.Drawing.Point(118, 357);
+            this.RegisterDescription.Name = "RegisterDescription";
+            this.RegisterDescription.Size = new System.Drawing.Size(229, 13);
+            this.RegisterDescription.TabIndex = 11;
+            this.RegisterDescription.Text = "If you don\'t have an account please click here:";
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RegisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterButton.Location = new System.Drawing.Point(121, 373);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(223, 29);
+            this.RegisterButton.TabIndex = 12;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // LoginPageBack
+            // 
+            this.LoginPageBack.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginPageBack.AutoSize = true;
+            this.LoginPageBack.BackColor = System.Drawing.Color.Transparent;
+            this.LoginPageBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LoginPageBack.FlatAppearance.BorderSize = 0;
+            this.LoginPageBack.ForeColor = System.Drawing.Color.Transparent;
+            this.LoginPageBack.Image = global::UserLogin.Properties.Resources.icons8_back_arrow_50;
+            this.LoginPageBack.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LoginPageBack.Location = new System.Drawing.Point(12, 12);
+            this.LoginPageBack.Name = "LoginPageBack";
+            this.LoginPageBack.Size = new System.Drawing.Size(33, 33);
+            this.LoginPageBack.TabIndex = 13;
+            this.LoginPageBack.UseVisualStyleBackColor = false;
+            this.LoginPageBack.Click += new System.EventHandler(this.LoginPageBack_Click);
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.LoginButton;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.LoginPageBack);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.RegisterDescription);
+            this.Controls.Add(this.LoginDescription);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.LoginPage);
+            this.Controls.Add(this.Forgot_password);
+            this.Controls.Add(this.pwdtxtbox);
+            this.Controls.Add(this.nametxtbox);
+            this.Controls.Add(this.LoginButton);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diet Tracker v.0.1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox nametxtbox;
+        private System.Windows.Forms.TextBox pwdtxtbox;
+        private System.Windows.Forms.LinkLabel Forgot_password;
+        private System.Windows.Forms.Label LoginPage;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label LoginDescription;
+        private System.Windows.Forms.Label RegisterDescription;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Button LoginPageBack;
+    }
+}
+
