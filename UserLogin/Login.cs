3a55f8852c5fbc19a2a3deaef27c10b76399a0ca
Quagon <jonash_msn@hotmail.com>
2018-04-13 17:41:17 +0200

    Fixede tilfælde af, at der manglede en database

--- UserLogin/Login.cs
+++ UserLogin/Login.cs
@@ -83,101 +83,109 @@ namespace Login
             else
             {
                 // MYSQL CODE to receive Usernames from the database
-                MySqlConnection myUserConnection = new MySqlConnection();
-                myUserConnection.ConnectionString = "database=diettracker;server=localhost;user id=ApplicationAccess;";
-
-                MySqlCommand UserCommand = new MySqlCommand();
-                UserCommand.CommandText = "SELECT Username FROM username WHERE Username = '" + user + "';";
-                UserCommand.Connection = myUserConnection;
-                myUserConnection.Open();
-                MySqlDataReader UsernameRead = UserCommand.ExecuteReader();
-
-                /* C# CODE to assign the username equal to the data in the database
-                I use a try here instead of an if, since if the MySqlDataReader can't find the written name in the database it crashes.
-                Further down an exception handles that*/
                 try
                 {
-                    UsernameRead.Read();
-                    string Username = UsernameRead.GetString(0);
-                    var UserDatabase = String.Format("{0}", Username);
-                    Username = UserDatabase;
+                    MySqlConnection myUserConnection = new MySqlConnection();
+                    myUserConnection.ConnectionString = "database=diettracker;server=localhost;user id=ApplicationAccess;";
 
-                    //Now that we know Username is correct, we begin checking the password and if its empty 
+                    MySqlCommand UserCommand = new MySqlCommand();
+                    UserCommand.CommandText = "SELECT Username FROM username WHERE Username = '" + user + "';";
+                    UserCommand.Connection = myUserConnection;
+                    myUserConnection.Open();
+                    MySqlDataReader UsernameRead = UserCommand.ExecuteReader();
 
+                    /* C# CODE to assign the username equal to the data in the database
+                    I use a try here instead of an if, since if the MySqlDataReader can't find the written name in the database it crashes.
+                    Further down an exception handles that*/
+                    try
                     {
-                        if (string.IsNullOrEmpty(pass))
+                        UsernameRead.Read();
+                        string Username = UsernameRead.GetString(0);
+                        var UserDatabase = String.Format("{0}", Username);
+                        Username = UserDatabase;
+
+                        //Now that we know Username is correct, we begin checking the password and if its empty 
+
                         {
-                            MessageBox.Show("Please enter your password!");
-                            return false;
-                        }
-
-                        //check password is valid (This is related to being able to input tekst inside a password)
-                        else if (StringValidator(pass) == true)
-                        {
-                            MessageBox.Show("You can't use special characters");
-                            ClearTexts(user, pass);
-                            return false;
-                        }
-
-                        // MYSQL CODE: SELECT password FROM password, username WHERE password.ID = username.ID AND username.Username = 'User';
-                        // C# code til at sætte password; pass = password received from MySql 
-
-                        myUserConnection.Close();
-                        ClearTexts(user, pass);
-                        MySqlConnection myPasswordConnection = new MySqlConnection();
-                        myPasswordConnection.ConnectionString = "database=diettracker;server=localhost;user id=ApplicationAccess;";
-
-                        MySqlCommand PasswordCommand = new MySqlCommand();
-                        PasswordCommand.CommandText = "SELECT Password FROM username, password WHERE password.ID = username.ID AND username = '" + user + "';";
-                        PasswordCommand.Connection = myPasswordConnection;
-                        myPasswordConnection.Open();
-                        MySqlDataReader PasswordRead = PasswordCommand.ExecuteReader();
-
-                        //Even though, for some reason, the system doesn't crash if you input a non-existing password, i still use a try just to be sure nothing goes wrong
-                        try
-                        {
-                            PasswordRead.Read();
-                            //MessageBox.Show();
-                            string Userpassword = PasswordRead.GetString(0);
-                            var PasswordDatabase = String.Format("{0}", Userpassword);
-                            Userpassword = PasswordDatabase;
-
-                            //Since a password was entered, we check if the password is correct 
-                            if (Userpassword != pass)
+                            if (string.IsNullOrEmpty(pass))
                             {
-                                MessageBox.Show("Incorrect Password");
+                                MessageBox.Show("Please enter your password!");
+                                return false;
+                            }
+
+                            //check password is valid (This is related to being able to input tekst inside a password)
+                            else if (StringValidator(pass) == true)
+                            {
+                                MessageBox.Show("You can't use special characters");
                                 ClearTexts(user, pass);
+                                return false;
+                            }
+
+                            // MYSQL CODE: SELECT password FROM password, username WHERE password.ID = username.ID AND username.Username = 'User';
+                            // C# code til at sætte password; pass = password received from MySql 
+
+                            myUserConnection.Close();
+                            ClearTexts(user, pass);
+                            MySqlConnection myPasswordConnection = new MySqlConnection();
+                            myPasswordConnection.ConnectionString = "database=diettracker;server=localhost;user id=ApplicationAccess;";
+
+                            MySqlCommand PasswordCommand = new MySqlCommand();
+                            PasswordCommand.CommandText = "SELECT Password FROM username, password WHERE password.ID = username.ID AND username = '" + user + "';";
+                            PasswordCommand.Connection = myPasswordConnection;
+                            myPasswordConnection.Open();
+                            MySqlDataReader PasswordRead = PasswordCommand.ExecuteReader();
+
+                            //Even though, for some reason, the system doesn't crash if you input a non-existing password, i still use a try just to be sure nothing goes wrong
+                            try
+                            {
+                                PasswordRead.Read();
+                                //MessageBox.Show();
+                                string Userpassword = PasswordRead.GetString(0);
+                                var PasswordDatabase = String.Format("{0}", Userpassword);
+                                Userpassword = PasswordDatabase;
+
+                                //Since a password was entered, we check if the password is correct 
+                                if (Userpassword != pass)
+                                {
+                                    MessageBox.Show("Incorrect Password");
+                                    ClearTexts(user, pass);
+                                    myPasswordConnection.Close();
+                                    return false;
+                                }
+                                else
+                                {
+                                    //Once we managed to get to this part of the code, we know both the Username and the Password was correct
+                                    myPasswordConnection.Close();
+                                    return true;
+                                }
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Unusual error; Incorrect Password");
                                 myPasswordConnection.Close();
                                 return false;
                             }
-                            else
-                            {
-                                //Once we managed to get to this part of the code, we know both the Username and the Password was correct
-                                myPasswordConnection.Close();
-                                return true;
-                            }
-                        }
-                        catch
-                        {
-                            MessageBox.Show("Unusual error; Incorrect Password");
-                            myPasswordConnection.Close();
-                            return false;
                         }
                     }
+
+                    //As written above, in case the user doesn't exist in the database, the reader can't handle it, so instead this exception catches it and resets it all
+                    catch
+                    {
+                        MessageBox.Show("That Username Doesn't exist");
+                        ClearTexts(user, pass);
+                        myUserConnection.Close();
+                        return false;
+
+                    }
+                    finally
+                    {
+                        myUserConnection.Close();
+                    }
                 }
-            
-                //As written above, in case the user doesn't exist in the database, the reader can't handle it, so instead this exception catches it and resets it all
                 catch
                 {
-                    MessageBox.Show("That Username Doesn't exist");
-                    ClearTexts(user, pass);
-                    myUserConnection.Close();
+                    MessageBox.Show("No connection to Database");
                     return false;
-
-                }
-                finally
-                {
-                    myUserConnection.Close();
                 }
             }
         }
