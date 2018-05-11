6a9ae960be69d5f20da92fab7f2b03725bcd99f6
Christian(Atlik/Gunder) <atlikamv@hotmail.com>
2018-05-11 03:25:55 +0200

    Graf design tilføjet

--- MainPageGraphs/MainPageGraphs/MainPageGraphs/Form1.cs
+++ MainPageGraphs/MainPageGraphs/MainPageGraphs/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MainPageGraphs
 {
@@ -19,6 +20,32 @@ namespace MainPageGraphs
 
         private void chart1_Click(object sender, EventArgs e)
         {
+            DateTime date = new DateTime(2018-04-06);
+            var info = new  UpdateWeightGraph(new DateTime(2018-04-05), 72);
+            var info1 = new UpdateWeightGraph(new DateTime(2018-04-05), 72);
+            var info2 = new UpdateWeightGraph(date, 71);
+
+            //får forkert string ud???
+            var dateStr = Convert.ToString(date);
+           // var dateStr2 = DateTime.ToString(date);
+
+            weightOverTimeChart.Series["Weight"].Points.AddXY("2018-04-05", 72);
+            weightOverTimeChart.Series["Weight"].Points.AddXY(dateStr, 72);
+            weightOverTimeChart.Series["Weight"].Points.AddXY(Convert.ToString(info2.DayOfInput), info2.Weight);
+        }
+
+        //tænker på at tilføje denne således at MySQL bliver indlæst herfra istedet for fra en separat metode
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void caloriesChart_Click(object sender, EventArgs e)
+        {
+            var info = new UpdateCaloriesGraph(2600, 2220);
+
+            caloriesChart.Series["CalorieIntake"].Points.AddXY("Goal", info.Goal);
+            caloriesChart.Series["CalorieIntake"].Points.AddXY("Calorie intake", info.Calories);
 
         }
     }
---
+++ MainPageGraphs/MainPageGraphs/MainPageGraphs/UpdateCaloriesGraph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainPageGraphs
+{
+    class UpdateCaloriesGraph
+    {
+        internal int Calories { get; set; }
+        internal int Goal { get; set; }
+
+        internal UpdateCaloriesGraph(int goal, int calories)
+        {
+            this.Calories = calories;
+            this.Goal = goal;
+        }
+    }
+}
